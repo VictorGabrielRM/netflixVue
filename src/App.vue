565a36116260636f4330cbbdf40506471f051ff0
Victor Gabriel Ribeiro <victorgabryel@hotmail.com>
2018-06-24 11:34:02 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h2>Meu componente! Projeto: {{ nomeProjeto }}</h2>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      nomeProjeto: 'Netflix com vue'
+    }
+  },
 }
 </script>
 
